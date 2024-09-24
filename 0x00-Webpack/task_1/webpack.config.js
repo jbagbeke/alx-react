47b5b2fc983783e86e24d2705250d2d44378f75e
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-09-24 21:37:00 +0000

    0x00-Webpack Mandatory Task 1

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,12 +1,7 @@
 import path from 'path';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 
-export default {
+module.exports = {
     mode: 'production',
     entry: {
         bundle: path.resolve(__dirname, './js/dashboard_main.js')
