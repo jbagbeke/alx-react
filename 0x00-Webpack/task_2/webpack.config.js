756532515f612a9177c9b280a4c2b3420b993250
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-09-25 21:00:39 +0000

    0x00-Webpack Mandatory Task 2

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -9,9 +9,6 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public')
     },
-    performance: {
-		maxAssetSize: 1000000,
-	},
     module: {
         rules: [
             {
