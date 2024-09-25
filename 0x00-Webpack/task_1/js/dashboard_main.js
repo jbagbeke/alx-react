bf610d017a83251694a1957bad8cecf94986b144
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-09-25 15:45:54 +0000

    0x00-Webpack Mandatory Task 1

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,9 +1,11 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
+const button = $("<button>Click here to get started</button>")
+
 $('body').append("<p>Holberton Dashboard</p>");
 $('body').append("<p>Dashboard data for the students</p>");
-$('body').append("<button>Click here to get started</button>");
+$('body').append(button);
 $('body').append("<p id='count'></p>");
 $('body').append("<p>Copyright - Holberton School</p>");
 
@@ -16,4 +18,4 @@ function updateCounter() {
 }
 
 
-$('button').on('click', _.debounce(updateCounter, 500))
+$(button).on('click', _.debounce(updateCounter, 500))
