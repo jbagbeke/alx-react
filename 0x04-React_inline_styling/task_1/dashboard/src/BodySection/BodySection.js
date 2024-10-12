459aecce4adbe40a2ac5eddeab2f824ac0a3df35
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-10-12 20:18:48 +0000

    0x03. React component Task 1

--- 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySection.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySection.js
@@ -6,7 +6,7 @@ export default class BodySection extends Component {
     render() {
         const { title, children } = this.props;
         return (
-            <div className="bodySection">
+            <div className={css(BodySectionStyles.bodySection)}>
               <h2>{title}</h2>
               {children}
             </div>
@@ -21,3 +21,9 @@ BodySection.propTypes = {
 BodySection.defaultProps = {
     title: ""
 }
+
+const BodySectionStyles = StyleSheet.create({
+    bodySection: {
+        border: '3px solid yellow'
+    }
+})
