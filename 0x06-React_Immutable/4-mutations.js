4b1781aaf5bb4905360032f7d21e21179bdec11e
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-10-23 22:12:57 +0000

    0x06. React Immutable Mandatory Task 4

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -9,10 +9,4 @@ export const map = Map({
     6: 'Lucas',
 })
 
-export const map2 = map.setIn([2], 'Benjamin').setIn([4], 'Oliver')
-
-
-map.forEach((v, k) => console.log(k, ' ----- ', v))
-console.log()
-console.log()
-map2.forEach((v, k) => console.log(k, ' ----- ', v))
+export const map2 = map.setIn([2], 'Benjamin').setIn([4], 'Olivia')
