4a8d4835a228e91568700b3ff354fab1a48ca0d4
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-10-23 22:54:19 +0000

    0x06. React Immutable Mandatory Task 5

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,11 +1,13 @@
 import { List, Map } from 'immutable';
 
 export function concatElements(page1, page2) {
-    return List(Map(page1).concat(page2))
+    const p1 = List(page1)
+    const p2 = List(page2)
+    return p1.concat(p2)
 }
 
 export function mergeElements(page1, page2) {
     if (page1 === page2)
-        return page2
+        return List(page2)
     return List(Map(page1).merge(page2))
 }
