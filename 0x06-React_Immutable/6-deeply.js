3b3f97ca84fccf9d1877d7024a239949ddbe2d28
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-10-23 23:23:19 +0000

    0x06. React Immutable Mandatory Task 7

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -4,28 +4,3 @@ export default function mergeDeeplyElements(page1, page2) {
     return Map(page1).mergeDeep(page2)
 }
 
-
-
-const page1 = {
-    'user-1': {
-      id: 1,
-      name: 'test',
-      likes: {
-        1: {
-          uid: 1234,
-        }
-      }
-    },
-  };
-  
-  const page2 = {
-    'user-1': {
-      likes: {
-        2: {
-          uid: 134,
-        }
-      }
-    },
-  };
-  
-console.log(mergeDeeplyElements(page1, page2).toJS());
